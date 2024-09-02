601803d942a4acc7b8fa909d6eeb1628252f4a61
lucasdulci <luqui00713@gmail.com>
2024-09-02 19:46:11 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { useReducer, useEffect, useMemo } from "react";
+import { Form } from "./components/Form";
+import { activityReducer, initialState } from "./reducers/activityReducer";
+import { ActivityList } from "./components/ActivityList";
+import { CalorieTracker } from "./components/CalorieTracker";
+
+function App() {
+  const [state, dispatch] = useReducer(activityReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("activities", JSON.stringify(state.activities));
+  }, [state.activities])
+
+  const canRestartApp = () => useMemo(() => state.activities.length, [state.activities]) 
+
+  return (
+    <>
+      <header className='bg-lime-800 py-3'>
+        <div className='max-w-4xl mx-auto flex justify-between'>
+          <h1 className='text-center text-lg font-bold uppercase text-white'>
+            contador de calorias
+          </h1>
+
+          <button
+            className='bg-gray-800 hover:bg-gray-900 p-2  before:font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10 disabled:cursor-auto'
+            disabled={!canRestartApp()}
+            onClick={() => dispatch({type: 'restart-app'})}
+            > 
+            Reiniciar app
+          </button>
+        </div>
+      </header>
+
+      <section className='bg-lime-500 py-20 px-5'>
+        <div className='max-w-4xl mx-auto'>
+          <Form
+            dispatch={dispatch}
+            state={state}
+          />
+        </div>
+      </section>
+
+      <section className="bg-gray-800 py-10">
+          <div className="max-w-4xl mx-auto">
+              <CalorieTracker
+                activities={state.activities}
+              />
+          </div>
+      </section>
+
+      <section className='p-10 mx-auto max-w-4xl'>
+        <ActivityList
+          activities={state.activities}
+          dispatch={dispatch}
+        />
+      </section>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/CalorieTracker.tsx
@@ -0,0 +1,58 @@
+import { useMemo } from "react";
+import { Activity } from "../types";
+import { CaloriesDisplay } from "./CaloriesDisplay";
+
+type CalorieTrackerProps = {
+  activities: Activity[];
+};
+
+export const CalorieTracker = ({ activities }: CalorieTrackerProps) => {
+  // contadores
+  const caloriesConsumed = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.category === 1 ? total + activity.calories : total,
+        0
+      ),
+    [activities]
+  );
+
+  const caloriesBurned = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.category === 2 ? total + activity.calories : total,
+        0
+      ),
+    [activities]
+  );
+
+  const netCalories = useMemo(
+    () => caloriesConsumed - caloriesBurned,
+    [activities]
+  );
+  return (
+    <>
+      <h2 className='text-4xl font-black text-white text-center'>
+        Resumen de Calorias
+      </h2>
+      <div className='flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10'>
+        <CaloriesDisplay
+          calories={caloriesConsumed}
+          text='Consumidas'
+        />
+
+        <CaloriesDisplay
+          calories={caloriesBurned}
+          text='Ejercicio'
+        />
+
+        <CaloriesDisplay
+          calories={netCalories}
+          text='Diferencia'
+        />
+      </div>
+    </>
+  );
+};
